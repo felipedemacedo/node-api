924afd7642af02e27aeb732f10cfcf6db65c9e10
Felipe de Macêdo Rodrigues <felipe.de.rodrigues@accenture.com>
2019-02-24 17:10:44 -0300

    using queryParser restify plugin

--- main.ts
+++ main.ts
@@ -1,15 +1,38 @@
-import * as restify from 'restify'
+import * as restify from "restify";
 
 const server = restify.createServer({
-    name: 'api',
-    version: '1.0.0'
-})
+  name: "api",
+  version: "1.0.0"
+});
 
-server.get('/hello', (req, resp, next)=>{
-    resp.json({message: 'hello'})
-    return next()
-})
+// plugin para capturar parâmetros de url:
+server.use(restify.plugins.queryParser());
 
-server.listen(3000, ()=>{
-    console.log('api is running on http://localhost:3000')
-})
+// http://localhost:3000/info?param1=value1&param2=value2
+server.get("/info", [
+  (req, resp, next) => {
+    if (req.userAgent() && req.userAgent().includes("Edge")) {
+      resp.status(400);
+      resp.json({
+        message: "This functionality does not work correctly on EDGE Broser."
+      });
+      return next(false);
+    }
+    return next();
+  },
+  (req, resp, next) => {
+    resp.status(400);
+    resp.json({
+      browser: req.userAgent(),
+      method: req.method,
+      url: req.url,
+      path: req.path(),
+      query: req.query
+    });
+    return next();
+  }
+]);
+
+server.listen(3000, () => {
+  console.log("api is running on http://localhost:3000");
+});
