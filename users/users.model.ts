36e056e4b6235211346ba14a1fafa599426d77e6
Felipe de Macêdo Rodrigues <felipe.de.rodrigues@accenture.com>
2019-02-24 22:10:25 -0300

    find by id

--- users/users.model.ts
+++ users/users.model.ts
@@ -1,10 +1,23 @@
 const users = [
-    {name: 'user 1', email: 'firs@mail.com'},
-    {name: 'user 2', email: 'second@mail.com'}
+    {id: '1', name: 'user 1', email: 'first@mail.com'},
+    {id: '2', name: 'user 2', email: 'second@mail.com'}
 ]
 
 export class User {
+
     static findAll(): Promise<any[]>{
         return Promise.resolve(users)
     }
+
+    static findById(id: string): Promise<any>{
+        return new Promise(resolve=>{
+            const filtered = users.filter(user => user.id === id)
+            let user = undefined
+            if (filtered.length > 0){
+                user = filtered[0]
+            }
+            resolve(user)
+        })
+    }
+    
 }
